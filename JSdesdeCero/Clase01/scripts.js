1e627de048dbc4ff288af6d1f0fba919fda9248f
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-07-25 23:28:39 -0500

    0.0.2

--- JSdesdeCero/Clase01/scripts.js
+++ JSdesdeCero/Clase01/scripts.js
@@ -1,3 +1,16 @@
 alert('Hola Mundo')
 let nombre = "Luigi"
 console.log(nombre)
+
+let numero=18;
+console.log(numero);
+console.log(numero + 1);
+console.log(numero/3);
+
+let edad = 22, cargo="Front End Developer", pais="Peru";
+console.log(edad);
+console.log(cargo);
+console.log(pais);
+
+const frase = "Nunca dejar de aprender";
+console.log(frase);
