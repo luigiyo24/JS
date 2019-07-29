0e45722d344a9926eaaa784f75ea108df4b9dcc1
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-07-28 22:27:36 -0500

    0.4.0

---
+++ JSdesdeCero/Clase04/scripts.js
@@ -0,0 +1,10 @@
+function saludar(persona){
+    return 'Bienvenido a EDTeam, ' + persona    ;
+}
+
+console.log(saludar('Beto'));
+
+const saludo = (persona) => {
+    return 'Bienvenido ' + persona;
+}
+console.log(saludo('Luigi'));
