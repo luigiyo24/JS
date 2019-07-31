e2d43976c8768e36addce37e929f82476fab9e22
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-07-30 23:14:02 -0500

    0.5.0

--- JSdesdeCero/Clase04/scripts.js
+++ JSdesdeCero/Clase04/scripts.js
@@ -8,3 +8,14 @@ const saludo = (persona) => {
     return 'Bienvenido ' + persona;
 }
 console.log(saludo('Luigi'));
+
+const sumarTodos = (...numeros) => {
+    let resultado = 0;
+    for(let i= 0; i<numeros.length;i++){
+        resultado += numeros[i];
+        // console.log(numeros[i]);
+    }
+    return resultado;
+}
+
+console.log(sumarTodos(1,2,3,4,5,6));
---
+++ JSdesdeCero/Clase05/scripts.js
@@ -0,0 +1,17 @@
+//CREAR ARRAY
+let arr = [1,2,3,4];
+//AGREGAR VALOR A UN ARRAY
+arr.push(5);
+//QUITAR ELEMENTO DE UN ARRAY
+arr.pop();
+//AGREGAR VALOR AL INICIO DE UN ARRAY
+arr.unshift('Nuevo');
+//QUITAR EL PRIMER VALOR DE UN ARRAY
+arr.shift();
+//AGREGAR ARRAY EN CUALQUIER POSICION
+let arrayp = ['PERU','COLOMBIA','BOLIVIA','MEXICO'];
+
+arrayp.splice(2,0,'CHILE','ECUADOR')
+
+
+
