e5215f29524c0ba0a030c0e4fbb22a426c57c6da
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-07-31 00:35:33 -0500

    0.5.1

--- JSdesdeCero/Clase05/scripts.js
+++ JSdesdeCero/Clase05/scripts.js
@@ -10,8 +10,22 @@ arr.unshift('Nuevo');
 arr.shift();
 //AGREGAR ARRAY EN CUALQUIER POSICION
 let arrayp = ['PERU','COLOMBIA','BOLIVIA','MEXICO'];
-
+//AGREGA UN ELEMENTO Y PERMITE ELIMINAR UN ELEMENTO
 arrayp.splice(2,0,'CHILE','ECUADOR')
 
+'Hola mama'.split('');
+
+'Hola mama'.split('').reverse();
+
+const reverseText = string => string.split('').reverse().join('');
+
+reverseText('Luigi');
 
 
+let abc = ['B','D','A','C'];
+abc.sort();
+abc.sort().reverse();
+
+let numbers = [2,10,20,35,45,100,300,1000];
+
+numbers.sort((a,b) => a - b);
