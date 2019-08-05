802c19039fd15278b6037b931007d80fe7e4f37d
luigi alonso yovera torres <luigi.yo_24@hotmail.com>
2019-08-04 23:11:34 -0500

    2.1.0

---
+++ JSdesdeCero/Clase06/scripts.js
@@ -0,0 +1,12 @@
+    let perro = {
+        nombre : "Boris",
+        edad: 3,
+        color : "negro",
+        sexo : "macho",
+        correr(){
+            console.log(`${this.nombre} corre`);
+        }
+    }
+
+    console.log(perro);
+    perro.nombre;
